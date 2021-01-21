484aaa3bf2d9986d214068ddc414de6360baa43c
Gonzalo <gonzaloguimerans@gmail.com>
2021-01-21 16:59:32 +0100

    Terminadas plantillas html

--- movements/views.py
+++ movements/views.py
@@ -5,17 +5,19 @@ import sqlite3
 from datetime import date, datetime
 
 @app.route("/")
-def portada():
+def movimientosCrypto():
     conn = sqlite3.connect("movements/data/bbdd_crypto.db")
     c = conn.cursor()
     c.execute("SELECT date, time, from_currency, from_quantity, to_currency, to_quantity, from_quantity/to_quantity FROM tabla_movimientos;")
 
     movimientos = c.fetchall()
 
-    conn.close()
-
-    return render_template("portada.html", movimientos=movimientos) 
-
+    if not movimientos:
+            conn.close()
+            return render_template("sin_movimientos.html") 
+    else:    
+        conn.close()
+        return render_template("movimientos.html", movimientos=movimientos) 
     
 
 @app.route("/compra", methods=["GET", "POST"])
@@ -25,7 +27,7 @@ def compraCrypto():
     if request.method == "POST":
 
         now = datetime.now()
-        fecha = now.strftime('%Y/%m/%d')
+        fecha = now.strftime('%Y-%m-%d')
         hora = now.strftime("%H:%M:%S")
 
         conn = sqlite3.connect("movements/data/bbdd_crypto.db")
@@ -33,7 +35,8 @@ def compraCrypto():
 
         c.execute("INSERT INTO tabla_movimientos (date, time, from_currency, from_quantity, to_currency, to_quantity) VALUES (?,?,?,?,?,?);",
                 (
-                    fecha, hora, 
+                    fecha, 
+                    hora, 
                     request.form.get("fromMoneda"),
                     float(request.form.get("cantidadFrom")),
                     request.form.get("toMoneda"),
@@ -44,11 +47,16 @@ def compraCrypto():
         conn.commit()
         conn.close()
 
-        return redirect(url_for("portada"))
+        return redirect(url_for("movimientosCrypto"))
           
 
     return render_template("compra.html", monedas=monedas)
 
-@app.route("/status", methods=["GET"])
+@app.route("/estado", methods=["GET"])
 def statusCrypto():
+    conn = sqlite3.connect("movements/data/bbdd_crypto.db")
+    c = conn.cursor()
+    c.execute("SELECT from_currency, from_quantity, to_currency, to_quantity, FROM tabla_movimientos;")
+
+    
     return render_template("estado.html")
