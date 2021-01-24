a53fae7183bd6f892cd825909e33de82c7edeec8
Gonzalo <gonzaloguimerans@gmail.com>
2021-01-24 17:43:19 +0100

    a falta de una operacion de valor de krypto

--- movements/views.py
+++ movements/views.py
@@ -51,12 +51,93 @@ def compraCrypto():
           
 
     return render_template("compra.html", monedas=monedas)
+    
+
 
 @app.route("/estado", methods=["GET"])
 def statusCrypto():
     conn = sqlite3.connect("movements/data/bbdd_crypto.db")
     c = conn.cursor()
-    c.execute("SELECT from_currency, from_quantity, to_currency, to_quantity, FROM tabla_movimientos;")
 
     
-    return render_template("estado.html")
+    c.execute("SELECT M.importe_destino - M2.importe_origen as saldoEuros "
+                "FROM "
+                "(SELECT coalesce(sum(to_quantity), 0) as importe_destino "
+                "FROM tabla_movimientos "
+                "WHERE to_currency = 'EUR') as M, "
+                "(SELECT coalesce(sum(from_quantity), 0) as importe_origen "
+                "FROM tabla_movimientos "
+                "WHERE from_currency = 'EUR') as M2;"
+            )
+
+    consultaSaldo = c.fetchone()
+
+    saldoEuros = 0
+
+    if consultaSaldo:
+        saldoEuros = consultaSaldo[0]
+
+    c.execute("""SELECT coalesce(sum(from_quantity), 0)
+                FROM tabla_movimientos 
+                WHERE from_currency = 'EUR';"""
+            )    
+    consultaTotal = c.fetchone()
+
+    totalEuros = 0
+
+    if consultaTotal:
+        totalEuros = consultaTotal[0]
+
+
+    c.execute("""SELECT coalesce(sum(to_quantity), 0) as importe_destino, to_currency
+                FROM tabla_movimientos
+                WHERE to_currency != 'EUR'
+                GROUP BY to_currency;"""
+            )
+    consultaToAll = c.fetchall()
+
+    c.execute("""SELECT coalesce(sum(from_quantity), 0) as importe_origen, from_currency
+                FROM tabla_movimientos
+                WHERE from_currency != 'EUR'
+                GROUP BY from_currency;"""
+            )
+    consultaFromAll = c.fetchall()
+
+    listaCrypto = []
+
+    if consultaToAll:
+        if consultaFromAll:
+            for registroTo in consultaToAll:
+                saldoCrypto = registroTo[0]
+                monedaTo = registroTo[1]
+                for registroFrom in consultaFromAll:
+                    monedaFrom = registroFrom[1]
+                    if monedaTo == monedaFrom:
+                        saldoCrypto = registroTo[0] - registroFrom[0]
+                        break
+            
+                listaCrypto.append((saldoCrypto, monedaTo))
+
+    if consultaFromAll:
+        if consultaToAll:
+            for registroFrom in consultaFromAll:
+                saldoCrypto = registroFrom[0]*(-1)
+                monedaFrom = registroFrom[1]
+                encontrado = False
+                for registroTo in consultaToAll:
+                    monedaTo = registroTo[1]
+                    if monedaTo == monedaFrom:
+                        encontrado=True
+                        break 
+                if encontrado == False:
+                    listaCrypto.append((saldoCrypto, monedaFrom))
+    print(listaCrypto)        
+    
+    
+    
+    
+    conn.close()
+
+    return render_template("estado.html", saldoEuros=saldoEuros, totalEuros=totalEuros)
+
+
