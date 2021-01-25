d1f75528ca9cbb8c0f2efb6cafbfa370e976a8e2
Gonzalo <gonzaloguimerans@gmail.com>
2021-01-25 16:15:59 +0100

    pendiente de restricciones y refactorizacion

--- movements/views.py
+++ movements/views.py
@@ -4,9 +4,22 @@ import csv
 import sqlite3
 from datetime import date, datetime
 
+DBFILE = "movements/data/bbdd_crypto.db"
+
+def consultaBD(query, params):
+    conn = sqlite3.connect(DBFILE)
+    c = conn.cursor()    
+
+    c.execute(query, params)
+
+    conn.commit()
+    conn.close()
+
+    return c.fetchall()
+
 @app.route("/")
 def movimientosCrypto():
-    conn = sqlite3.connect("movements/data/bbdd_crypto.db")
+    conn = sqlite3.connect(DBFILE)
     c = conn.cursor()
     c.execute("SELECT date, time, from_currency, from_quantity, to_currency, to_quantity, from_quantity/to_quantity FROM tabla_movimientos;")
 
@@ -19,7 +32,7 @@ def movimientosCrypto():
         conn.close()
         return render_template("movimientos.html", movimientos=movimientos) 
     
-
+# pendiente cálculo de P.U.
 @app.route("/compra", methods=["GET", "POST"])
 def compraCrypto():
     monedas = ("EUR", "BTC", "ETH", "XRP", "LTC", "BCH", "BNB", "USDT", "EOS", "BSV", "XLM", "ADA", "TRX")
@@ -30,7 +43,7 @@ def compraCrypto():
         fecha = now.strftime('%Y-%m-%d')
         hora = now.strftime("%H:%M:%S")
 
-        conn = sqlite3.connect("movements/data/bbdd_crypto.db")
+        conn = sqlite3.connect(DBFILE)
         c = conn.cursor()
 
         c.execute("INSERT INTO tabla_movimientos (date, time, from_currency, from_quantity, to_currency, to_quantity) VALUES (?,?,?,?,?,?);",
@@ -56,10 +69,11 @@ def compraCrypto():
 
 @app.route("/estado", methods=["GET"])
 def statusCrypto():
-    conn = sqlite3.connect("movements/data/bbdd_crypto.db")
+    conn = sqlite3.connect(DBFILE)
     c = conn.cursor()
 
-    
+
+#Consulta de saldo de Euros invertidos    
     c.execute("SELECT M.importe_destino - M2.importe_origen as saldoEuros "
                 "FROM "
                 "(SELECT coalesce(sum(to_quantity), 0) as importe_destino "
@@ -77,6 +91,7 @@ def statusCrypto():
     if consultaSaldo:
         saldoEuros = consultaSaldo[0]
 
+#Consulta de total de Euros invertidos
     c.execute("""SELECT coalesce(sum(from_quantity), 0)
                 FROM tabla_movimientos 
                 WHERE from_currency = 'EUR';"""
@@ -89,6 +104,8 @@ def statusCrypto():
         totalEuros = consultaTotal[0]
 
 
+
+#Consulta listado de cryptomonedas y sus valores
     c.execute("""SELECT coalesce(sum(to_quantity), 0) as importe_destino, to_currency
                 FROM tabla_movimientos
                 WHERE to_currency != 'EUR'
@@ -131,13 +148,20 @@ def statusCrypto():
                         break 
                 if encontrado == False:
                     listaCrypto.append((saldoCrypto, monedaFrom))
-    print(listaCrypto)        
     
     
-    
-    
-    conn.close()
+    #Consulta saldo Cryptomonedas: pendiente conexión con API para hacer conversión real (saldoCryptoenEuros)
+    saldoCrypto = 0
+    for registroCrypto in listaCrypto:    
+        saldoCrypto += registroCrypto[0]
 
-    return render_template("estado.html", saldoEuros=saldoEuros, totalEuros=totalEuros)
+    saldoCryptoenEuros = saldoCrypto*2
+
+    #Valor actual: cálculo de Total de euros invertidos + Saldo de euros invertidos (ganancia/perdida) + Valor de euros de nuestras cryptos (inversión atrapada)
+
+    valorActual = totalEuros + saldoEuros + saldoCryptoenEuros
+
+    conn.close()
+    return render_template("estado.html", totalEuros=totalEuros, valorActual=valorActual)
 
 
