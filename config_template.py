fb615676538ac8bddd51e381a40ec85512cad961
Gonzalo <gonzaloguimerans@gmail.com>
2021-02-05 14:38:56 +0100

    previo subida AWS

--- config_template.py
+++ config_template.py
@@ -1,2 +1,4 @@
 SECRET_KEY = "clave conocida"
 DBFILE = "ruta de la BBDD"
+API_KEY = "clave privada"
+API_URL = " https://pro-api.coinmarketcap.com/v1/tools/price-conversion?amount={}&symbol={}&convert={}&CMC_PRO_API_KEY={}"
